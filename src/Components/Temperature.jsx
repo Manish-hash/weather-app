3e09d925f4bf2f356fbcec7bc69562b59856a031
Manish <manishtimalsina51@gmail.com>
2024-04-28 18:48:18 +0545

    make temperature component and adding the property of search the city

---
+++ src/Components/Temperature.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+function Temperature() {
+  const handleCityChange = (e) => {
+    console.log(e.target.value);
+  };
+  return (
+    <>
+      <div className="flex justify-between">
+        <input
+          type="search"
+          className="bg-slate-600 border border-slate-500 text-slate-200 placeholder-slate-400 text-md w-60 p-2 focus:outline-none focus:border-slate-400"
+          placeholder="Enter Your City Name"
+          onChange={handleCityChange}
+        />
+        <div className="m-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="white"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+            />
+          </svg>
+        </div>
+      </div>
+
+      <div>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+</svg>
+
+      </div>
+    </>
+  );
+}
+export default Temperature;
